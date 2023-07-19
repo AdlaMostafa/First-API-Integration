08475a2c2637b0fc29a1eed9742c8d343a1d17ac
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-19 16:52:44 +0300

    Adding AboutPage

---
+++ src/Pages/AboutPage/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+// import AboutSection from '../../components/AboutSection'
+
+const AboutPage =()=>{
+  return (
+    <div>
+      {/* <Header/> */}
+      <h1>About Page</h1>
+      {/* <AboutSection/> */}
+      {/* <Footer/> */}
+    </div>
+  )
+}
+export default AboutPage;
