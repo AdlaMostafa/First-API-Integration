0935ee35458260d32a052d0645067d4e91c1a548
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-19 18:42:31 +0300

    Updating EditPostPage

--- src/Pages/EditPostPage/index.jsx
+++ src/Pages/EditPostPage/index.jsx
@@ -6,32 +6,31 @@ import PostForm from '../../components/PostForm';
 import { PATHS } from '../../router/paths';
 import { Navigate } from 'react-router-dom';
 
-const EditPostPage = () => {
+const EditPostPage = ({ params }) => {
+  const id = params?.id;
   const [post, setPost] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isGotToListPage, setIsGotToListPage] = useState(false);
 
-  useEffect(async () => {
-    try {
-      const { data } = await axios.get(
-        `https://some-data.onrender.com/stores/${this.id}`
-      );
-      setPost(data);
-      console.log(data);
-    } catch (error) {
-      console.log({ error: error.message });
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(`https://some-data.onrender.com/stores/${id}`);
+        setPost(data);
+        console.log(data);
+      } catch (error) {
+        console.log({ error: error.message });
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [id]);
 
   const handleEditPost = async (body) => {
     setIsLoading(true);
     try {
-      const res = await axios.put(
-        `https://some-data.onrender.com/stores/${this.id}`,
-        body
-      );
+      const res = await axios.put(`https://some-data.onrender.com/stores/${id}`, body);
       console.log(res.data);
       setPost(res.data);
       setIsLoading(false);
@@ -44,12 +43,9 @@ const EditPostPage = () => {
   return (
     <div>
       <Container>
-        <h1>Edit Post {this.id}</h1>
-        <PostForm
-          post={post}
-          handleSubmit={handleEditPost}
-          isLoading={isLoading}
-        />
+        <h1>Edit Post {id}</h1>
+
+        <PostForm post={post} handleSubmit={handleEditPost} isLoading={isLoading} />
       </Container>
 
       {isGotToListPage && <Navigate to={PATHS.POSTS.ROOT} replace />}
