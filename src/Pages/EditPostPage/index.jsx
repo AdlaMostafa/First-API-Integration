e796958f1a88b9b69b30d724eace9765035646fc
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-19 17:48:46 +0300

    Adding EditPostPage Code

--- src/Pages/EditPostPage/index.jsx
+++ src/Pages/EditPostPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import WithParams from '../../components/WithParams';
 import Container from '../../components/Container';
@@ -6,70 +6,55 @@ import PostForm from '../../components/PostForm';
 import { PATHS } from '../../router/paths';
 import { Navigate } from 'react-router-dom';
 
-class EditPostPage extends Component {
-  state = {
-    post: null,
-    isLoading: true,
-    isGotToListPage: false,
-  };
+const EditPostPage = () => {
+  const [post, setPost] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isGotToListPage, setIsGotToListPage] = useState(false);
 
-  id = this.props.params.id;
-  
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get(
+        `https://some-data.onrender.com/stores/${this.id}`
+      );
+      setPost(data);
+      console.log(data);
+    } catch (error) {
+      console.log({ error: error.message });
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
-  async componentDidMount() {
-    // fetch(`https://some-data.onrender.com/stores/${this.id}`)
-    //   .then((response) => response.json())
-    //   .then((data) => this.setState({ post: data, isLoading: false }));
-    try{
-       const {data}= await axios.get(`https://some-data.onrender.com/stores/${this.id}`);
-       this.setState({post:data});
-       console.log(data);
-    }
-    catch (error){
-      console.log({error:error.message});
-    }
-    finally{
-      this.setState({isLoading:false})
-    }
-  }
-
-  handleEditPost = async (body) => {
-    this.setState({ isLoading: true });
+  const handleEditPost = async (body) => {
+    setIsLoading(true);
     try {
       const res = await axios.put(
         `https://some-data.onrender.com/stores/${this.id}`,
         body
       );
       console.log(res.data);
-      this.setState({
-        post: res.data,
-        isLoading: false,
-        isGotToListPage: true,
-      });
+      setPost(res.data);
+      setIsLoading(false);
+      setIsGotToListPage(true);
     } catch (error) {
       console.log(error.message);
     }
   };
 
-  render() {
-    return (
-      <div>
-        <Container>
-          <h1>Edit Post {this.id}</h1>
+  return (
+    <div>
+      <Container>
+        <h1>Edit Post {this.id}</h1>
+        <PostForm
+          post={post}
+          handleSubmit={handleEditPost}
+          isLoading={isLoading}
+        />
+      </Container>
 
-          <PostForm
-            post={this.state.post}
-            handleSubmit={this.handleEditPost}
-            isLoading={this.state.isLoading}
-          />
-        </Container>
-
-        {this.state.isGotToListPage && (
-          <Navigate to={PATHS.POSTS.ROOT} replace />
-        )}
-      </div>
-    );
-  }
-}
+      {isGotToListPage && <Navigate to={PATHS.POSTS.ROOT} replace />}
+    </div>
+  );
+};
 
 export default WithParams(EditPostPage);
