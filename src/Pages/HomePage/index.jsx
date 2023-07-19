2f4e47a9ab559cdc692758b5c80da6ec4a6c92ef
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-19 17:52:50 +0300

    Adding HomePage Code

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Link } from 'react-router-dom';
 
 import { PATHS } from '../../router/paths';
-
-class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-        <p>
-          <Link to={PATHS.ABOUT}>learn more...</Link>
-        </p>
-      </div>
-    );
-  }
+const HomePage =()=>{
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        <Link to={PATHS.ABOUT}>learn more...</Link>
+      </p>
+    </div>
+  );
 }
+
 
 export default HomePage;
