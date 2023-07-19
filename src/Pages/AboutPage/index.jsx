152c04647c568f2d3b6289b33be6e19d7a09bcbe
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-19 17:20:15 +0300

    Adding Code Of About Page

--- src/Pages/AboutPage/index.jsx
+++ src/Pages/AboutPage/index.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-// import AboutSection from '../../components/AboutSection'
 
 const AboutPage =()=>{
   return (
     <div>
       {/* <Header/> */}
       <h1>About Page</h1>
-      {/* <AboutSection/> */}
       {/* <Footer/> */}
     </div>
   )
