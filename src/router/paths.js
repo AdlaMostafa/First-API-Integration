59f34d2da75055a31a0282dca3db4e951d6b7c1e
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-22 11:06:02 +0300

    Updating paths

--- src/router/paths.js
+++ src/router/paths.js
@@ -1,14 +1,12 @@
 export const PATHS = {
     HOME: '/',
     ABOUT: '/about',
-    COUNTERS:'/counters',
     POSTS: {
       ROOT: '/posts',
       VIEW: `/posts/:id`,
       CREATE: '/posts/create',
       EDIT: '/posts/:id/edit',
     },
-  
     ERRORS: {
       NOT_FOUND: '404',
     },
