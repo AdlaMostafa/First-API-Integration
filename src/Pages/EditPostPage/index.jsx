4e42338c18703854cb3cd7e88001954df7658242
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-22 10:53:43 +0300

    Update EditPostPage by using useNavigate Hooks

--- src/Pages/EditPostPage/index.jsx
+++ src/Pages/EditPostPage/index.jsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import WithParams from '../../components/WithParams';
-import Container from '../../components/Container';
-import PostForm from '../../components/PostForm';
-import { PATHS } from '../../router/paths';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import WithParams from "../../components/WithParams";
+import Container from "../../components/Container";
+import PostForm from "../../components/PostForm";
+import { PATHS } from "../../router/paths";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const EditPostPage = ({ params }) => {
   const id = params?.id;
   const [post, setPost] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [isGotToListPage, setIsGotToListPage] = useState(false);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data } = await axios.get(`https://some-data.onrender.com/stores/${id}`);
+        const { data } = await axios.get(
+          `https://some-data.onrender.com/stores/${id}`
+        );
         setPost(data);
         console.log(data);
       } catch (error) {
@@ -30,7 +32,10 @@ const EditPostPage = ({ params }) => {
   const handleEditPost = async (body) => {
     setIsLoading(true);
     try {
-      const res = await axios.put(`https://some-data.onrender.com/stores/${id}`, body);
+      const res = await axios.put(
+        `https://some-data.onrender.com/stores/${id}`,
+        body
+      );
       console.log(res.data);
       setPost(res.data);
       setIsLoading(false);
@@ -44,11 +49,13 @@ const EditPostPage = ({ params }) => {
     <div>
       <Container>
         <h1>Edit Post {id}</h1>
-
-        <PostForm post={post} handleSubmit={handleEditPost} isLoading={isLoading} />
+        <PostForm
+          post={post}
+          handleSubmit={handleEditPost}
+          isLoading={isLoading}
+        />
       </Container>
-
-      {isGotToListPage && <Navigate to={PATHS.POSTS.ROOT} replace />}
+      {isGotToListPage && navigate(PATHS.POSTS.ROOT)}
     </div>
   );
 };
