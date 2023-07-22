536aa30fe2d21ebe69db7b5e856a2d354e3e0c7d
Adla Mostafa <adlamostafa422004@gmail.com>
2023-07-22 11:02:53 +0300

    Updationg Router By using UseRoutes Hooks and summarize the code

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,37 +1,9 @@
-import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-
-import HomePage from "../Pages/HomePage";
-import AboutPage from "../Pages/AboutPage";
-import PostsPage from "../Pages/PostsPage";
-import PostPage from "../Pages/PostPage";
-import EditPostPage from "../Pages/EditPostPage";
-import CreatePostPage from "../Pages/CreatePostPage";
-
-import { PATHS } from "./paths";
+import { useRoutes } from 'react-router-dom';
+import {routes} from './routes'
 
 const Router = () => {
-  return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path={PATHS.ABOUT} element={<AboutPage />} />
-      <Route path={PATHS.POSTS.ROOT} element={<Outlet />}>
-        <Route index element={<PostsPage />} />
-        <Route path={PATHS.POSTS.VIEW} element={<PostPage />} />
-        <Route path={PATHS.POSTS.EDIT} element={<EditPostPage />} />
-        <Route path={PATHS.POSTS.CREATE} element={<CreatePostPage />} />
-      </Route>
-
-      <Route
-        path={PATHS.ERRORS.NOT_FOUND}
-        element={<h1>Page not found 404</h1>}
-      />
-
-      <Route
-        path="*"
-        element={<Navigate to={PATHS.ERRORS.NOT_FOUND} replace={true} />}
-      />
-    </Routes>
-  );
+  const router = useRoutes(routes);
+  return router;
 };
 
 export default Router;
